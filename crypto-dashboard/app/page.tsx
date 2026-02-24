0fc76a633949e9a3b39d26e77657ad4875589af8
AiswaryaKMohanan <aiswaryak227@gmail.com>
2026-02-24 23:01:50 +0100

    integrate CoinGecko API and implement CoinCard dashboard view

--- crypto-dashboard/app/page.tsx
+++ crypto-dashboard/app/page.tsx
@@ -1,6 +1,9 @@
+import CoinCard from "./components/CoinCard";
 import Navbar from "./components/Navbar";
+import { fetchCoins } from "./services/api";
 
-export default function Home() {
+export default async function Home() {
+  const coins = await fetchCoins()
   return (
     <div>
       <main className=" bg-white dark:bg-black sm:items-start">
@@ -9,6 +12,13 @@ export default function Home() {
           <h2 className="text-2xl font-semibold">
           Market Overview
         </h2>
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          {coins.map((coin)=>{
+            return(<div key={coin.id}>
+              <CoinCard coin={coin}></CoinCard>
+            </div>)
+          })}
+        </div>
         </div>
       </main>
     </div>
