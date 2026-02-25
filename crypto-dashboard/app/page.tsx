f7cbb4839bfff9f0f04e8819768cbd8918fe0e50
AiswaryaKMohanan <aiswaryak227@gmail.com>
2026-02-25 22:35:54 +0100

    feat: implement client-side search and gainers/losers filtering with optimized memoization

--- crypto-dashboard/app/page.tsx
+++ crypto-dashboard/app/page.tsx
@@ -1,26 +1,20 @@
 import CoinCard from "./components/CoinCard";
+import CoinList from "./components/CoinList";
 import Navbar from "./components/Navbar";
 import { fetchCoins } from "./services/api";
 
 export default async function Home() {
   const coins = await fetchCoins()
-  return (
-    <div>
-      <main className=" bg-white dark:bg-black sm:items-start">
-        <Navbar />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h2 className="text-2xl font-semibold">
+return (
+    <main>
+      <Navbar />
+      <div className="p-6 max-w-6xl mx-auto">
+        <h2 className="text-2xl font-semibold mb-6">
           Market Overview
         </h2>
-        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {coins.map((coin)=>{
-            return(<div key={coin.id}>
-              <CoinCard coin={coin}></CoinCard>
-            </div>)
-          })}
-        </div>
-        </div>
-      </main>
-    </div>
+
+        <CoinList coins={coins} />
+      </div>
+    </main>
   );
 }
